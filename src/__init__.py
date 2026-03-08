25e74e1e1c90331607d7d20f43fad3a42038972f
veeravaishnavi2007-wq <veeravaishnavi2007@gmail.com>
2026-03-08 16:25:53 +0530

    Create src/__init__.py with hybrid adaptive edge-aware image enhancement algorithm description

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""Hybrid Adaptive Edge-Aware Image Enhancement Algorithm for Portable X-ray Devices"""
+
+__version__ = "1.0.0"
+__author__ = "veeravaishnavi2007-wq"
+
+from .enhancement_pipeline import HybridXrayEnhancementPipeline
+
+__all__ = ["HybridXrayEnhancementPipeline"]
